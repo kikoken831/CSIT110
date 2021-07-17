7db4e684fa451ccc2bfb16b384a3d254f7e661e9
System Administrator <root@Kendricks-MacBook-Air.local>
2021-07-17 21:05:54 +0800

    Applied Prettier on codebase

--- Assignment2/Kendrick_Kee_kk135_a2.py
+++ Assignment2/Kendrick_Kee_kk135_a2.py
@@ -26,14 +26,14 @@ def question_2():
     sum = 0
     # go forwards
     print("Go forward: ", end='')
-    while(i <= upperB):#loop so long as counter is not above the upper bound
+    while(i <= upperB):  # loop so long as counter is not above the upper bound
         print(str(i), end="")
-        sum = sum + i#get the sum
-        i = i+gap#increment counter by gap amount
-        if(i <= upperB):#trailing comma when not end of line
+        sum = sum + i  # get the sum
+        i = i+gap  # increment counter by gap amount
+        if(i <= upperB):  # trailing comma when not end of line
             trailing = ", "
         else:
-            trailing = '.'#trailing . when end of line
+            trailing = '.'  # trailing . when end of line
         print(trailing, end='')
     print()
     print("Sum of numbers = " + str(sum))
@@ -41,80 +41,82 @@ def question_2():
     sum = 0
     # go backwards
     print("Go backward: ", end='')
-    while(i >= 0):#loop so long as counter is greater than 0
+    while(i >= 0):  # loop so long as counter is greater than 0
         print(str(i), end='')
-        sum = sum + i#get the sum
-        i = i-gap#decrement counter by gap amount
+        sum = sum + i  # get the sum
+        i = i-gap  # decrement counter by gap amount
         if(i > -1):
-            trailing = ", "#trailing with a comma when not end of line
+            trailing = ", "  # trailing with a comma when not end of line
         else:
-            trailing = '.'#trailing . when end of line
+            trailing = '.'  # trailing . when end of line
         print(trailing, end='')
     print()
     print("Sum of numbers = " + str(sum))
-    
 
 
 def question_3():
     # ====insert solution here===#]
     total_int = 0.0
-    age = int(input("Enter current age: "))#store age
-    oa = float(input("Enter current amount in OA: "))#store OA
-    sa = float(input("Enter current amount in SA: "))#store SA
-    ma = float(input("Enter current amount in MA: "))#store MA
-    #get basic interest for OA,SA,MA
+    age = int(input("Enter current age: "))  # store age
+    oa = float(input("Enter current amount in OA: "))  # store OA
+    sa = float(input("Enter current amount in SA: "))  # store SA
+    ma = float(input("Enter current amount in MA: "))  # store MA
+    # get basic interest for OA,SA,MA
     total_int += oa*0.025
     total_int += sa * 0.04
     total_int += ma * 0.04
-    #check if age is above or equal 55
+    # check if age is above or equal 55
     if(age >= 55):
-        ra = float(input("Enter current amount in RA: "))#prompt for RA when user is older than 55
-        total_int += ra*0.04#store RA
-        if(oa > 20000):#normalize OA to 20000 as extra interest calculations for OA is capped at 20000
+        # prompt for RA when user is older than 55
+        ra = float(input("Enter current amount in RA: "))
+        total_int += ra*0.04  # store RA
+        if(oa > 20000):  # normalize OA to 20000 as extra interest calculations for OA is capped at 20000
             oa = 20000
-        total_sum = oa+sa+ma+ra#get the total sum for extra interest computation with normalized OA
-        if(total_sum >= 60000):#if total sum is >= 60k offer maximum interest of $900
+        # get the total sum for extra interest computation with normalized OA
+        total_sum = oa+sa+ma+ra
+        if(total_sum >= 60000):  # if total sum is >= 60k offer maximum interest of $900
             total_int += 900
-        elif(total_sum >= 30000):#if total sum is >= 30k but not >60k
-            total_int += 600#offer the 2% interest on the first 30k and 1% on the remainder amount
-            total_int += ((total_sum%30000)*0.01)#get 1% of the remainder of the total sum
+        elif(total_sum >= 30000):  # if total sum is >= 30k but not >60k
+            total_int += 600  # offer the 2% interest on the first 30k and 1% on the remainder amount
+            # get 1% of the remainder of the total sum
+            total_int += ((total_sum % 30000)*0.01)
         else:
             total_int += (total_sum*0.02)
-            
-    else:#if age is below 55
-        if(oa > 20000):#normalize the OA as its also capped at 20k
+
+    else:  # if age is below 55
+        if(oa > 20000):  # normalize the OA as its also capped at 20k
             oa = 20000
-        total_sum = oa+sa+ma#get total sum
-        if(total_sum >= 60000):#if total sum > 60k give maximum interest of $600
+        total_sum = oa+sa+ma  # get total sum
+        if(total_sum >= 60000):  # if total sum > 60k give maximum interest of $600
             total_int += 600
-        else:#else give 1% on whatever is left
+        else:  # else give 1% on whatever is left
             total_int += total_sum*0.01
 
     print(f'Your interest rate this year will be ${total_int:.2f}')
 
 
 def question_4():
-  #====insert solution here===#
-  finalString = ""
-  while(True):#loop until a blank line is detected
-    x = input("Filename?")
-    if(x == ""):#when a blankline is detected break out of loop
-        finalString = finalString[:-1]#delete the last trailing comma 
-        break
-    flag = 0#set a flag, when flag is 0, store the char as it is not nested
-    for i in x:
-      if i == '[':#if its an opening bracket, set flag to 1 so as no to read any chars till a closing bracket is found
-        flag += 1
-      elif i == ']' and flag > 0:#if a closing brace is found AND flag is 1 or more(nesting) reduce the flag by 1
-        flag -= 1
-      elif flag == 0:#if flag is 0, all nests are closed, store the char in the placeholder
-        finalString += i #append to the cleaned string to the final string output
-    flag = 0
+    #====insert solution here===#
+    finalString = ""
+    while(True):  # loop until a blank line is detected
+        x = input("Filename?")
+        if(x == ""):  # when a blankline is detected break out of loop
+            finalString = finalString[:-1]  # delete the last trailing comma
+            break
+        flag = 0  # set a flag, when flag is 0, store the char as it is not nested
+        for i in x:
+            if i == '[':  # if its an opening bracket, set flag to 1 so as no to read any chars till a closing bracket is found
+                flag += 1
+            # if a closing brace is found AND flag is 1 or more(nesting) reduce the flag by 1
+            elif i == ']' and flag > 0:
+                flag -= 1
+            elif flag == 0:  # if flag is 0, all nests are closed, store the char in the placeholder
+                finalString += i  # append to the cleaned string to the final string output
+        flag = 0
 
-    finalString += ","#append a trailing comma if it was a valid input
+        finalString += ","  # append a trailing comma if it was a valid input
 
-         
-  print(finalString)
+    print(finalString)
 
 
 def main():  # do not change this line
